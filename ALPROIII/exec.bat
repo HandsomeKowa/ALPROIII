b597cb5fa3546ab773b620560d0f08c3f12908f4
Matheus <matheus.souza@acad.pucrs.br>
2013-04-19 20:12:24 -0300

    Revert "Adição dos códigos já prontos"
    This reverts commit 83497f87ff8519d96e1443920b595d469baddac4.

--- ALPROIII/exec.bat
+++
@@ -1,9 +0,0 @@
-echo Iniciando a execucao
-cls
-del result.txt
-REM result.txt apagado
-REM Iniciando a execucao do job
-trabalho.exe >> result.txt
-REM job finalizado com sucesso
-REM Exibindo o resultado
-result.txt
